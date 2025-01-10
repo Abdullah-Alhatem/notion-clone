a66902941c26836ada50c79516eafb1a5ea83b19
Abdullah Al-hatem <abdhatm42@gmail.com>
2025-01-10 08:57:19 +0300

    Repairs in .collaboration-cursor__caret{margin-left:0px;margin-right:0px;} in blocknote Modules (some style problem)

--- app/doc/[id]/page.tsx
+++ app/doc/[id]/page.tsx
@@ -16,3 +16,33 @@ function DocumentPage({
   );
 }
 export default DocumentPage;
+
+
+
+
+
+
+// ------------------------------------- حل لمشكله next.js 15 ---------------------------------------------
+// "use client";
+// import React from "react"; // تأكد من استيراد React
+// import Document from "@/components/Document";
+
+// function DocumentPage({
+//   params,
+// }: {
+//   params: {
+//     id: string;
+//   };
+// }) {
+//   // تحويل params إلى Usable<{ id: string }> باستخدام React.use
+//   const unwrappedParams = React.use<{ id: string }>(Promise.resolve(params));
+//   const { id } = unwrappedParams; // استخراج id من params المفكوكة
+
+//   return (
+//     <div className="flex flex-1 flex-col min-h-screen">
+//       <Document id={id} />
+//     </div>
+//   );
+// }
+
+// export default DocumentPage;
