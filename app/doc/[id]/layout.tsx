8bd655cdb0f191fbe40bb814d17b85fda9238640
Abdullah Al-hatem <abdhatm42@gmail.com>
2025-02-28 21:41:57 +0300

    more repairs

--- app/doc/[id]/layout.tsx
+++ app/doc/[id]/layout.tsx
@@ -1,17 +1,43 @@
+// import RoomProvider from "@/components/RoomProvider";
+// import { auth } from "@clerk/nextjs/server";
+
+// async function DocLayout({
+//   children,
+//   params: { id },
+// }: {
+//   children: React.ReactNode;
+//   params: {
+//     id: string;
+//   };
+// }) {
+//   await auth.protect();
+
+
+//   return <RoomProvider roomId={id}>{children}</RoomProvider>;
+// }
+// export default DocLayout;
+
 import RoomProvider from "@/components/RoomProvider";
 import { auth } from "@clerk/nextjs/server";
 
+interface DocLayoutProps {
+  children: React.ReactNode;
+  params: Promise<{
+    id: string;
+  }>;
+}
+
 async function DocLayout({
   children,
-  params: { id },
-}: {
-  children: React.ReactNode;
-  params: {
-    id: string;
-  };
-}) {
+  params,
+}: DocLayoutProps) {
+  // Await the params object to get the id
+  const resolvedParams = await params;
+  const { id } = resolvedParams;
+  
   await auth.protect();
 
   return <RoomProvider roomId={id}>{children}</RoomProvider>;
 }
+
 export default DocLayout;
